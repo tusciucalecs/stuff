c39d37149845942c7a418696339658f3beb7878f
Alecs Tusciuc <tusciucalecs@gmail.com>
2013-06-11 17:57:36 +0300

    Preparing to produce data

--- bbob/javabbob/Experiment.java
+++ bbob/javabbob/Experiment.java
@@ -33,7 +33,7 @@ public class Experiment {
         /* Generate and evaluate the P0 population */
         for (int i = 0; i < np; i++) {
             for (int j = 0; j < dim; j++) {
-                population[i][j] = 10. * random.nextDouble() - 5.;
+                population[i][j] = 10. * random.nextDouble() - 4.;
             }
             fitness = fgeneric.evaluate(population[i]);
             if (fitness < fTarget) {
@@ -42,6 +42,7 @@ public class Experiment {
             populationFitness[i] = fitness;
         }
 
+        double progress = 1e5;
         for (double iter = 0; iter < maxfunevals; iter += np) {
             double[][] nextPopulation = new double[np][dim];
             double[] nextPopulationFitness = new double[np];
@@ -117,6 +118,20 @@ public class Experiment {
             }
             population = nextPopulation;
             populationFitness = nextPopulationFitness;
+            if (iter > progress) {
+                // System.out.println("----------------");
+                // System.out.printf("%.2e ", iter);
+                // System.out.println(fgeneric.getBest() + " din "
+                // + fgeneric.getFtarget());
+                // printDistribution(combinationsChances, total);
+
+                combinationsChances = Combination.initCombinations();
+                total = combinationsChances.size() * 10;
+                hits = new HashMap<Combination, Integer>();
+                nrHits = 0;
+
+                progress += 1e5;
+            }
         }
     }
 
@@ -141,15 +156,17 @@ public class Experiment {
     private static void printDistribution(Map<Combination, Integer> map,
             long total) {
         System.out.println("=================");
+        System.out.println(total);
         for (Combination combination1 : map.keySet()) {
             System.out.println(combination1 + " : " + map.get(combination1)
-                    * 100 / total + "%" + map.get(combination1));
+                    * 100 / total + "% as " + map.get(combination1));
         }
 
         System.out.println();
+        System.out.println(nrHits);
         for (Combination combination1 : hits.keySet()) {
             System.out.println(combination1 + " : " + hits.get(combination1)
-                    * 100 / nrHits + "% " + hits.get(combination1));
+                    * 100 / nrHits + "% as " + hits.get(combination1));
         }
     }
 
@@ -162,13 +179,15 @@ public class Experiment {
     public static void main(String[] args) {
 
         /* run variables for the function/dimension/instances loops */
-        final int dim[] = { 30, 2, 3, 5, 10, 20, 40 };
+        // final int dim[] = { 2, 3, 5, 10, 20, 40 };
+        final int dim[] = { 30 };
         final int instances[] = { 1, 2, 3, 4, 5, 31, 32, 33, 34, 35, 36, 37,
                 38, 39, 40 };
         /* Function indices are from 1 to 24 (noiseless) */
         // final int functions[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
         // 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
         final int functions[] = { 1, 2, 3, 4, 5, 15, 16, 17, 18, 19 };
+        // final int functions[] = { 16 };
         int idx_dim, idx_fun, idx_instances, independent_restarts;
         double maxfunevals;
         String outputPath;
@@ -191,11 +210,6 @@ public class Experiment {
         params.algName = "DDE";
         params.comments = "DDE Info";
         outputPath = "data";
-
-        if (args.length > 0) {
-            outputPath = args[0]; // Warning: might override the assignment
-                                  // above.
-        }
 
         /* Creates the folders for storing the experimental data. */
         if (JNIfgeneric.makeBBOBdirs(outputPath, false)) {
@@ -226,15 +240,17 @@ public class Experiment {
                             instances[idx_instances], dim[idx_dim], outputPath,
                             params);
                     /* Call to the optimizer with fgeneric as input */
-                    maxfunevals = 1e8 * dim[idx_dim];
+                    maxfunevals = 2e6 * dim[idx_dim];
                     independent_restarts = -1;
                     t0 = System.currentTimeMillis();
                     while (fgeneric.getEvaluations() < maxfunevals) {
                         independent_restarts++;
-                        DDE(fgeneric, dim[idx_dim], 5e6 * dim[idx_dim], rand);
+                        DDE(fgeneric, dim[idx_dim], 1e6 * dim[idx_dim], rand);
                         if (fgeneric.getBest() < fgeneric.getFtarget())
                             break;
-                        System.out.println("Restart");
+                        System.err.println("Restart " + independent_restarts
+                                + ": " + fgeneric.getBest() + " from "
+                                + fgeneric.getFtarget());
                     }
                     System.out.printf(
                             "f%d in %d-D, instance %d: FEs=%.2e, rr=%d",
